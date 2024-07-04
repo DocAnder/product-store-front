94a60d0d6389cc31afe53b6fb603cb6a16ebb614
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-04 17:03:13 -0300

    feat: create get one by id product service rote

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -14,8 +14,16 @@ export class ProductsService {
     return this.httpClient.get<Product[]>('http://127.0.0.1:8000/api/products');
   }
 
+  getOne(id: string){
+    return this.httpClient.get<Product>(`http://127.0.0.1:8000/api/products/${id}`);
+  }
+
   create(payload: ProductPayload){
     return this.httpClient.post('http://127.0.0.1:8000/api/products', payload);
+  }
+
+  update(id: string, payload: ProductPayload){
+    return this.httpClient.put(`http://127.0.0.1:8000/api/products/${id}`, payload);
   }
   
 }
