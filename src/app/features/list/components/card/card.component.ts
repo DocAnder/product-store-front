13f41a6544e7d078227a9b34ec452d1ef40cdc53
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-04 15:35:06 -0300

    feat: edit product page

--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, computed } from '@angular/core';
+import { Component, input, computed, Output, EventEmitter } from '@angular/core';
 import { MatCardModule } from "@angular/material/card"
 import { MatButtonModule } from "@angular/material/button"
 import { Product } from '../../../../shared/interfaces/product.interface';
@@ -14,7 +14,13 @@ export class CardComponent {
 
   product = input.required<Product>();
 
+  @Output() edit = new EventEmitter();
+
   productName = computed(() => this.product().name)
   productPrice = computed(() => this.product().price)
+
+  onEdit() {
+    this.edit.emit();
+  }
 
 }
