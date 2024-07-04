7fb844122d4e550d2a6d43a90d5f05e271bac1a1
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-03 21:01:40 -0300

    feat: create service method to save new product

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Product } from '../interfaces/product.interface';
+import { ProductPayload } from '../interfaces/payload-product.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class ProductsService {
 
   getAll(){
     return this.httpClient.get<Product[]>('http://127.0.0.1:8000/api/products');
+  }
+
+  create(payload: ProductPayload){
+    return this.httpClient.post('http://127.0.0.1:8000/api/products', payload);
   }
   
 }
