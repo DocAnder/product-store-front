9ea04fb0e0474b10efbbdc7eb71ba7cf63304778
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-05 22:15:20 -0300

    feat: implement image selector to create product

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule} from "@angular/material/form-field"
 import { MatInputModule } from "@angular/material/input"
 import { MatSnackBar } from "@angular/material/snack-bar"
 import { ProductsService } from '../../shared/services/products.service';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink, CommonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
@@ -18,6 +19,8 @@ export class CreateComponent {
 
   productsService = inject(ProductsService);
   snackBar = inject(MatSnackBar);
+  selectedFile!: File;
+  
 
   form = new FormGroup({
     name: new FormControl<string>('', {
@@ -32,26 +35,28 @@ export class CreateComponent {
       nonNullable:true,
       validators: Validators.required 
     }),
-    image: new FormControl<string>('', {
-      nonNullable:true,
-      validators: Validators.required 
-    }),
   });
 
+  onFileChange(event: any) {    
+    if (event.target.files.length > 0) {
+      this.selectedFile = event.target.files[0];
+    }
+  }
+
   onSubmit(){
-    this.productsService.create({
-      name: this.form.controls.name.value,
-      price: this.form.controls.price.value,
-      description: this.form.controls.description.value,
-      image: this.form.controls.image.value
-    })
+    const formData = new FormData();    
+    formData.append('name', this.form.controls.name.value)
+    formData.append('price', this.form.controls.price.value)
+    formData.append('description', this.form.controls.description.value)
+    formData.append('image', this.selectedFile);
+    this.productsService.createProductWhitImage(formData)
     .subscribe(() => {
-      this.snackBar.open('Produto Salvo!','', {
-        duration: 3000,
-        horizontalPosition: 'right',
-        verticalPosition: 'top'
-      });
-    });    
+        this.snackBar.open('Produto Salvo!','', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+        });
+      });   
   }
 
 }
